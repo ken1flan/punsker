3286462b1276a56dd36ef2236e60b2590597b42b
ken1flan <ken1flan@gmail.com>
2014-09-14 13:15:33 +0900

    refs #3 投稿の管理

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140913000001) do
+ActiveRecord::Schema.define(version: 20140914034305) do
 
   create_table "identities", force: true do |t|
     t.integer  "user_id"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20140913000001) do
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+
+  create_table "posts", force: true do |t|
+    t.integer  "user_id",       null: false
+    t.string   "body_text",     null: false
+    t.integer  "emotion_level", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"

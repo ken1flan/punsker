8f552b309c7987f1d9a3577db3f64f20de56bde9
ken1flan <ken1flan@gmail.com>
2014-09-15 11:31:03 +0900

    バリデーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,22 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-end
+
+  after_initialize :set_default_value
+
+  scope :order_newer, -> { order(:created_at) }
+
+  validates :user_id,
+    presence: true,
+    numericality: { only_integer: true, greater_than: 0, allow_blank: true }
+  validates :body_text,
+    presence: true,
+    length: { maximum: 255, allow_blank: true } 
+  validates :emotion_level,
+    presence: true,
+    numericality: { only_integer: true, allow_blank: true }
+
+  private
+    def set_default_value
+      self.emotion_level ||= 0
+    end
+ end
